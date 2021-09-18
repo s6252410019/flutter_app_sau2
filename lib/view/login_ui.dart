1fce631d7f7fd368c4535048b6f69197ca6ff8cc
s6252410019 <s6252410019@sau.ac.th>
2021-09-18 15:14:24 +0700

    CodeV.2 LoginUI

--- lib/view/login_ui.dart
+++ lib/view/login_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginUI extends StatefulWidget {
   const LoginUI({Key? key}) : super(key: key);
@@ -16,12 +17,15 @@ class _LoginUIState extends State<LoginUI> {
           child: Column(
             children: [
               SizedBox(
-                height: 10.0,
+                height: 80.0,
               ),
               Image.asset(
                 'assets/images/education.png',
-                //width: 150,
+                // width: 150.0,
                 width: MediaQuery.of(context).size.width * 0.5,
+              ),
+              SizedBox(
+                height: 10,
               ),
               RichText(
                 text: TextSpan(
@@ -29,7 +33,7 @@ class _LoginUIState extends State<LoginUI> {
                     TextSpan(
                       text: 'DTI',
                       style: TextStyle(
-                        color: Colors.deepPurpleAccent,
+                        color: Colors.purple,
                         fontFamily: 'Kanit',
                         fontSize: 35.0,
                         fontWeight: FontWeight.bold,
@@ -38,7 +42,25 @@ class _LoginUIState extends State<LoginUI> {
                     TextSpan(
                       text: '-',
                       style: TextStyle(
-                        color: Colors.orangeAccent,
+                        color: Colors.yellow,
+                        fontFamily: 'Kanit',
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'BC',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontFamily: 'Kanit',
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '-',
+                      style: TextStyle(
+                        color: Colors.yellow,
                         fontFamily: 'Kanit',
                         fontSize: 35.0,
                         fontWeight: FontWeight.bold,
@@ -47,36 +69,17 @@ class _LoginUIState extends State<LoginUI> {
                     TextSpan(
                       text: 'IoT',
                       style: TextStyle(
-                        color: Colors.redAccent,
+                        color: Colors.red,
                         fontFamily: 'Kanit',
                         fontSize: 35.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    TextSpan(
-                      text: '-',
-                      style: TextStyle(
-                        color: Colors.orangeAccent,
-                        fontFamily: 'Kanit',
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'SAU',
-                      style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontFamily: 'Kanit',
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
                   ],
                 ),
               ),
               Text(
-                'SoutEast Asia University',
+                'Southeast Asia University',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Kanit',
@@ -86,7 +89,7 @@ class _LoginUIState extends State<LoginUI> {
                 padding: const EdgeInsets.only(
                   left: 40.0,
                   right: 40.0,
-                  top: 30.0,
+                  top: 40.0,
                 ),
                 child: TextField(
                   decoration: InputDecoration(
@@ -97,7 +100,7 @@ class _LoginUIState extends State<LoginUI> {
                       ),
                       borderRadius: BorderRadius.circular(
                         10.0,
-                      ),                    
+                      ),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(
@@ -107,9 +110,8 @@ class _LoginUIState extends State<LoginUI> {
                       borderRadius: BorderRadius.circular(
                         10.0,
                       ),
-                      
                     ),
-                    labelText: 'Username',
+                    labelText: 'ชื่อผู้ใช้',
                     floatingLabelBehavior: FloatingLabelBehavior.always,
                     labelStyle: TextStyle(
                       fontFamily: 'Kanit',
@@ -124,9 +126,240 @@ class _LoginUIState extends State<LoginUI> {
                       Icons.person,
                       color: Color(0xFF101276),
                     ),
-                  
                   ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                  top: 20.0,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1.0,
+                        color: Color(0xFF101276),
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1.0,
+                        color: Color(0xFF101276),
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                    labelText: 'รหัสผ่าน',
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: TextStyle(
+                      fontFamily: 'Kanit',
+                      color: Color(0xFF101276),
+                    ),
+                    hintText: 'ไม่ต่ำกว่า 6 ตัวอักษร',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Kanit',
+                      color: Colors.grey[400],
+                    ),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Color(0xFF101276),
+                    ),
+                    suffixIcon: Icon(
+                      Icons.visibility_off,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                  top: 5.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'ลงทะเบียน',
+                          style: TextStyle(
+                            fontFamily: 'Kanit',
+                            color: Color(0xFF101276),
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          alignment: Alignment.centerLeft,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'ลืมรหัสผ่าน',
+                          style: TextStyle(
+                            fontFamily: 'Kanit',
+                            color: Colors.red,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          alignment: Alignment.centerRight,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text(
+                  'เข้าใช้งาน',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                    color: Color(0xFF101276),
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Color(0xFF101276),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                  top: 5.0,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 2.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 5.0,
+                        right: 5.0,
+                      ),
+                      child: Text(
+                        'หรือ',
+                        style: TextStyle(
+                          fontFamily: 'Kanit',
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 2.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.facebook,
+                ),
+                label: Text(
+                  'Facebook',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50,
+                    ),
+                  ),
+                  primary: Color(0xFF1877f2),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.googlePlusG,
+                ),
+                label: Text(
+                  'Google',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50,
+                    ),
+                  ),
+                  primary: Color(0xFFdb4437),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.apple,
+                ),
+                label: Text(
+                  'Apple ID',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50,
+                    ),
+                  ),
+                  primary: Color(0xFF333333),
+                ),
+              ),
+              SizedBox(
+                height: 60,
               ),
             ],
           ),
